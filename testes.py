8baaeaf073ea074a089ea92173f88b6fbe92354a
lacouth <lacouth@gmail.com>
2021-03-17 15:23:13 -0300

    trying to fix data_upload II

--- testes.py
+++ testes.py
@@ -6,5 +6,5 @@ HEROKU = 'https://tcc-julio.herokuapp.com'
 while True:
     response = requests.get(HEROKU + '/api/v1/data_download')
     if response.json():
-        print(response.json())
+        print(response)
     time.sleep(0.5)
