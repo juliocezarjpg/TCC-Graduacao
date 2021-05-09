f626cd293355dbfa5f163bda4dd2491c85efbf71
juliocezarjpg <juliocezarjpg@gmail.com>
2021-05-08 23:59:50 -0300

    imagem no banco de dados tentativa x

--- create_database.py
+++ create_database.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('database/database.db')
 cur = conn.cursor()
 
 cur.execute('CREATE TABLE status (id integer primary key, status integer);')
-cur.execute('INSERT INTO status VALUES(1,0)')
+cur.execute('INSERT INTO status VALUES(1,0);')
 
 cur.execute('CREATE TABLE stream (id integer primary key, stream TEXT);')
 cur.execute('INSERT INTO stream VALUES(1," ")')
