376e219812db55e8acd243d2ceca69f2c31083e5
juliocezarjpg <juliocezarjpg@gmail.com>
2021-05-08 22:35:27 -0300

    teste download image

--- static/track.js
+++ static/track.js
@@ -57,15 +57,15 @@ function runDetection() {
     .catch(error => console.log('error', error));
 
     console.log("Ola")
-    fetch("https://tcc-julio.herokuapp.com/api/v1/img_download",{method:'GET'})
-        .then(response => response.text())
-        .then(result => {
-            console.log("Tudo")
-            image.src = result
-            console.log("Bom?")
-            console.log(result)
-        })
-        .catch(error => console.log('error', error));
+    fetch("https://tcc-julio.herokuapp.com/api/v1/img_download",
+    {method:'GET'})
+    .then(response => response.text())
+    .then(result => {
+        console.log("Tudo")
+        //image.src = result
+        console.log(result)
+    })
+    .catch(error => console.log('error', error));
             
     model.detect(video).then(predictions => {
         if (predictions.length > 0){
